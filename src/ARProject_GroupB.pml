789985b39cb835e5cdec6e6f60f7b1a5336818bb
Mowstyl <indikalesken@hotmail.com>
2018-05-02 13:49:18 +0200

    Things working now.

---
+++ src/ARProject_GroupB.pml
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="ARProject_GroupB" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="SpeechCommands" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="ComeHere" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="FollowMe" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Faces" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="move" src="SpeechCommands/move.pmt" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
--- src/translations/translation_en_US.ts
+++ src/translations/translation_en_US.ts
@@ -2,91 +2,100 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
-        <name>Come Here/behavior.xar:/Say</name>
+        <name>ComeHere/behavior.xar:/Say</name>
         <message>
-            <source>Hola</source>
+            <location filename="ComeHere/behavior.xar" line="0"/>
+            <source>I've lost you</source>
             <comment>Text</comment>
-            <translation type="vanished">Hello</translation>
-        </message>
-        <message>
-            <location filename="Come Here/behavior.xar" line="0"/>
-            <source>i have lost you my love</source>
-            <comment>Text</comment>
-            <translation type="unfinished"></translation>
+            <translation type="unfinished">I've lost you</translation>
         </message>
     </context>
     <context>
         <name>Faces/behavior.xar:/Say</name>
         <message>
-            <source>Hola</source>
-            <comment>Text</comment>
-            <translation type="vanished">Hello</translation>
-        </message>
-        <message>
             <location filename="Faces/behavior.xar" line="0"/>
             <source>Hello, what is your name?</source>
             <comment>Text</comment>
-            <translation type="unfinished"></translation>
+            <translation type="unfinished">Hello, what is your name?</translation>
         </message>
     </context>
     <context>
         <name>Faces/behavior.xar:/Say (1)</name>
         <message>
-            <source>Hola</source>
-            <comment>Text</comment>
-            <translation type="vanished">Hello</translation>
-        </message>
-        <message>
             <location filename="Faces/behavior.xar" line="0"/>
             <source>Nice to meet you</source>
             <comment>Text</comment>
-            <translation type="unfinished"></translation>
+            <translation type="unfinished">Nice to meet you</translation>
         </message>
     </context>
     <context>
         <name>Faces/behavior.xar:/Say (2)</name>
         <message>
-            <source>Hola</source>
-            <comment>Text</comment>
-            <translation type="vanished">Hello</translation>
-        </message>
-        <message>
             <location filename="Faces/behavior.xar" line="0"/>
             <source>You are so ugly</source>
             <comment>Text</comment>
-            <translation type="unfinished"></translation>
+            <translation type="unfinished">You are so ugly</translation>
         </message>
     </context>
     <context>
-        <name>Follow Me/behavior.xar:/Say</name>
+        <name>FollowMe/behavior.xar:/Say</name>
         <message>
-            <location filename="Follow Me/behavior.xar" line="0"/>
             <source>i have lost you my love</source>
             <comment>Text</comment>
-            <translation type="unfinished"></translation>
+            <translation type="obsolete">i have lost you my love</translation>
         </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Say</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>Hola</source>
-            <comment>Text</comment>
-            <translation>Hello</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_2/behavior.xar:/Animated Say</name>
-        <message>
-            <source>Hola</source>
+            <source>Hello</source>
             <comment>Text</comment>
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_2/behavior.xar" line="0"/>
-            <source>No te entiendo amigo</source>
+            <source>I'm there, buddy</source>
             <comment>Text</comment>
-            <translation type="unfinished"></translation>
+            <translation type="obsolete">I'm there, buddy</translation>
+        </message>
+        <message>
+            <source>Meow</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Meow</translation>
+        </message>
+        <message>
+            <source>rero</source>
+            <comment>Text</comment>
+            <translation type="obsolete">rero</translation>
+        </message>
+        <message>
+            <location filename="FollowMe/behavior.xar" line="0"/>
+            <source>lelo</source>
+            <comment>Text</comment>
+            <translation type="unfinished">lelo</translation>
+        </message>
+    </context>
+    <context>
+        <name>SpeechCommands/behavior.xar:/Can't hear</name>
+        <message>
+            <location filename="SpeechCommands/behavior.xar" line="0"/>
+            <source>I can't hear ye</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I can't hear ye</translation>
+        </message>
+    </context>
+    <context>
+        <name>SpeechCommands/behavior.xar:/Default</name>
+        <message>
+            <location filename="SpeechCommands/behavior.xar" line="0"/>
+            <source>Praise the Sun</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Praise the Sun</translation>
+        </message>
+    </context>
+    <context>
+        <name>SpeechCommands/behavior.xar:/End</name>
+        <message>
+            <location filename="SpeechCommands/behavior.xar" line="0"/>
+            <source>Imma stop listening to you</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Imma stop listening to you</translation>
         </message>
     </context>
 </TS>
